aa55a45235fe2f9d71d65a7b86f096bd5c0f14e2
glitter <gercommand@gmail.com>
2018-01-23 21:59:18 +0800

    test car

--- libraries/MobileCar/MobileCar.h
+++ libraries/MobileCar/MobileCar.h
@@ -56,7 +56,7 @@ class MobileCar{
 
 			double velocityToPwm = 255 / MAX_VELOCITY;
 
-			rightPwm = (int)rightPid->execute(Vr_obj*velocityToPwm, Vr_fbk*velocityToPwm);
+			rightPwm = (int)rightPid->execute(Vr_obj*velocityToPwm, Vr_fbk*velocityToPwm);  // radius*Vr_obj*velocityToPwm?
 			leftPwm = (int)leftPid->execute(Vl_obj*velocityToPwm, Vl_fbk*velocityToPwm);
 
 			
